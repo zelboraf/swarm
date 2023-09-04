2de92f94a228940336e17b7bdcf104eb5776952f
zelboraf <uminski.krzysztof@gmail.com>
2023-09-04 15:50:03 +0200

    Initial commit

---
+++ src/main/java/edu/swarm/SwarmApplication.java
@@ -0,0 +1,13 @@
+package edu.swarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwarmApplication.class, args);
+	}
+
+}
